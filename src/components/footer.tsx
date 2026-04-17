a5b0344b0850db202c2cb69bd6a49c869642c6f9
Eric <kaiguaeric@gmail.com>
2026-04-17 16:41:55 +0300

    Footer and auth fixes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="flex flex-row mt-12 bg-gray-200 h-30 items-center justify-center rounded-sm ">
-        <p className="font-tertiary text-sm">@2026 <span className="text-accent">Lumina</span>Vision. A proposal product for showcasing.</p>
+        <p className="font-tertiary text-center text-sm">@2026 <span className="text-accent">Lumina</span>Vision. A proposal product for showcasing.</p>
     </footer>
   )
 }
